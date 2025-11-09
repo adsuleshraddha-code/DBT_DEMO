1be35110ec41c9d9473914604cdac43046bde7bd
adsule.shraddha@gmail.com <adsule.shraddha@gmail.com>
2025-11-09 08:05:47 +0000

    daily_weather model

---
+++ models/daily_weather.sql
@@ -0,0 +1,31 @@
+WITH daily_weather as (
+    select
+
+    date(time) as daily_weather,
+    weather,
+    temp,
+    pressure,
+    humidity,
+    clouds
+    
+     from 
+    {{ source('demo', 'weather') }}
+
+    limit 10
+),
+
+daily_weather_agg as (
+
+    select
+    daily_weather,
+    weather,
+    count(weather)
+    --row_number() over (partition by daily_weather order by count(weather) desc) as row_number
+
+    from daily_weather
+    group  by daily_weather,weather
+
+    qualify row_number() over (partition by daily_weather order by count(weather) desc) = 1
+)
+
+select * from daily_weather_agg --where row_number = 1
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,7 +3,9 @@ WITH CTE AS (
     STARTED_AT,
     to_timestamp(STARTED_AT) as STARTED_AT1,
     date(to_timestamp(STARTED_AT)) as date_timestamp,
-    hour(to_timestamp(STARTED_AT)) as hour_started_at
+    hour(to_timestamp(STARTED_AT)) as hour_started_at,
+    {{get_season('STARTED_AT')}} as STATION_OF_YEAR,
+    {{day_type('STARTED_AT')}} as DAY_TYPE
     
 
     FROM 
